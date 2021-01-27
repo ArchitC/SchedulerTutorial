f25b009690d2795b2771242b163dfb76a0e9a971
Archit Chopra <architchopra@Archits-MacBook-Pro.local>
2021-01-27 00:53:11 -0600

    course editing

--- App.js
+++ App.js
@@ -1,26 +1,37 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+
+import CourseEditScreen from './screens/CourseEditScreen';
 import ScheduleScreen from './screens/ScheduleScreen';
 import CourseDetailScreen from './screens/CourseDetailScreen';
+import UserContext from './UserContext';
 
 const Stack = createStackNavigator();
 
 const App = () => {
+  const [user, setUser] = useState({ role: 'admin'});
+
   return (
+    <UserContext.Provider value={user}>
     <NavigationContainer>
       <Stack.Navigator> 
         <Stack.Screen name="ScheduleScreen"
           component={ScheduleScreen}
           options={{ title: 'Schedule'}} 
         />
+        <Stack.Screen name="CourseEditScreen"
+            component={CourseEditScreen}
+            options={{ title: 'Course Editor'}} 
+          />
         <Stack.Screen name="CourseDetailScreen"
           component={CourseDetailScreen}
           options={{ title: 'Course detail'}} 
         />
       </Stack.Navigator>
     </NavigationContainer>
+    </UserContext.Provider>
   );
 };
 
--- screens/ScheduleScreen.js
+++ screens/ScheduleScreen.js
@@ -1,7 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { SafeAreaView, StyleSheet, Text} from 'react-native';
 
 import CourseList from '../components/CourseList';
+import UserContext from '../UserContext';
+import CourseEditScreen from './CourseEditScreen';
+
 
   
 const schedule = {
@@ -35,10 +38,12 @@ const Banner = ({title}) => (
 );
 
 const ScheduleScreen = ({navigation}) => {
+  const user = useContext(UserContext);
+  const canEdit = user && user.role === 'admin';
   const [schedule, setSchedule] = useState({ title: '', courses: [] });
 
   const view = (course) => {
-    navigation.navigate('CourseDetailScreen', { course });
+    navigation.navigate(canEdit ? 'CourseEditScreen' : 'CourseDetailScreen', { course });
   };
   
   const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
